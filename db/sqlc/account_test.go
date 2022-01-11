858e291ff5f8bac341d020aa431cbb508a6d15fe
Jeronimo Sanchez <jeronimo.sanchez@outlook.com>
2022-01-11 20:26:56 +0100

    Test UpdateAccount function

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -13,17 +13,35 @@ func TestCreateAccount(t *testing.T) {
 }
 
 func TestGetAccount(t *testing.T) {
-	createdAccount := createRandomAccount(t)
+	account := createRandomAccount(t)
 
-	gotAccount, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+	gotAccount, err := testQueries.GetAccount(context.Background(), account.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, gotAccount)
 
-	require.Equal(t, gotAccount.ID, createdAccount.ID)
-	require.Equal(t, gotAccount.Owner, createdAccount.Owner)
-	require.Equal(t, gotAccount.Balance, createdAccount.Balance)
-	require.Equal(t, gotAccount.Currency, createdAccount.Currency)
-	require.Equal(t, gotAccount.CreatedAt, createdAccount.CreatedAt)
+	require.Equal(t, gotAccount.ID, account.ID)
+	require.Equal(t, gotAccount.Owner, account.Owner)
+	require.Equal(t, gotAccount.Balance, account.Balance)
+	require.Equal(t, gotAccount.Currency, account.Currency)
+	require.Equal(t, gotAccount.CreatedAt, account.CreatedAt)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	updatedAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount)
+
+	require.Equal(t, updatedAccount.ID, account.ID)
+	require.Equal(t, updatedAccount.Balance, arg.Balance)
+	require.Equal(t, updatedAccount.Currency, account.Currency)
+	require.Equal(t, updatedAccount.CreatedAt, account.CreatedAt)
 }
 
 // Helpers
